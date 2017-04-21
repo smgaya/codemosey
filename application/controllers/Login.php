019e3494d60a4a474eb630817b0b6f998d21f08a
smgaya <smgayanath@gmail.com>
2017-04-21 22:16:53 +0300

    authentication

---
+++ application/controllers/Login.php
@@ -0,0 +1,11 @@
+<?php 
+class Login extends CI_Controller{
+	
+
+
+
+function index(){
+
+$this->load->view('login');
+}
+}
---
+++ application/views/login.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+	<title>
+		login
+	</title>
+</head>
+<body>
+	
+	<form>
+		
+		<input type="text"  name="username">
+		<input type="password" name="pass">
+		<input type="submit" name="login">
+	</form>
+</body>
+</html>
